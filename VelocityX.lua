829ccb210016dba2066499d663bbac2bc486a001
LunarSync1515 <constructorr1@gmail.com>
2026-03-08 16:14:53 +0100

    Update VelocityX.lua

--- VelocityX.lua
+++ VelocityX.lua
@@ -4129,9 +4129,9 @@ Library.Pages.Section = function(self, Data)
 
         -- Accent line
         Items["Liner"] = Instances:Create("Frame", {
-            Parent = Items["Topbar"].Instance,
-            Size = UDim2New(1,0,0,1),
-            Position = UDim2New(0,0,0,3),
+            Parent = Items["Section"].Instance,
+            Size = UDim2New(1, 0, 0, 1),
+            Position = UDim2New(0, 0, 0, 0),
             BorderSizePixel = 0,
             BackgroundColor3 = FromRGB(120,170,255)
         })  Items["Liner"]:AddToTheme({BackgroundColor3 = "Accent"})
@@ -5859,6 +5859,7 @@ Library.PlayerList = function(self)
 end
 
 return Library
+
 
 
 
