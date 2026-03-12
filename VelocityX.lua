58b472df011053382507ce7b9380dc00216c9e1c
LunarSync1515 <constructorr1@gmail.com>
2026-03-12 17:12:10 +0100

    update

--- VelocityX.lua
+++ VelocityX.lua
@@ -5464,7 +5464,7 @@ Library.CreateSettingsPage = function(self, Window, KeybindList, Watermark, Mode
 
         SettingsSection:Toggle({
             Name = "Target HUD",
-            Flag = "Target HUD",
+            Flag = "TargetHudEnabled",
             Default = true,
             Callback = function(Value)
                 if TargetHud then
@@ -5874,7 +5874,7 @@ Library.TargetHud = function(self)
             AnchorPoint = Vector2New(0.5, 0.5),
             Position = UDim2New(0.5, 0, 0.75, 0),
             BorderSizePixel = 0,
-            Size = UDim2New(0, 300, 0, 110),
+            Size = UDim2New(0, 340, 0, 130),
             BackgroundColor3 = FromRGB(16, 16, 18),
             Visible = true
         }) Items["TargetHud"]:AddToTheme({BackgroundColor3 = "Background 2"})
@@ -6365,6 +6365,7 @@ Library.TargetHud = function(self)
 end
 
 return Library
+
 
 
 
